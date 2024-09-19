1c3eae89b87a43120efa6493ae3062846c8a278c
AaronKKFX <aaron.k@aluno.ifsp.edu.br>
2024-09-19 12:14:43 -0300

    cabei

--- tests/sql_test.php
+++ tests/sql_test.php
@@ -2,9 +2,9 @@
     require_once '../core/sql.php';
 
     $id = 1;
-    $nome = 'murilo';
-    $email = 'murilo@gmail.com';
-    $senha = '123mudar';
+    $nome = 'Aaron';
+    $email = 'aaron@gmail.com';
+    $senha = '12345senha';
     $dados = ['nome' => $nome,
               'email' => $email,
               'senha' => $senha];
